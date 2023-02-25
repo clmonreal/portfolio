1cb00977af49b539cd860ec6e987eb69830e6094
Aquiles18 <c.lopemonre@gmail,com>
2023-02-25 20:35:21 +0100

    Redimensiona esfera de la seccion skills.

--- build/js/app.js
+++ build/js/app.js
@@ -22,35 +22,44 @@ window.addEventListener('scroll', function () {
 window.onload = function () {
   try {
     TagCanvas.textColour = '#000';
-    // TagCanvas.outlineColour = 'transparent';
     TagCanvas.maxSpeed = .1;
     TagCanvas.outlineColour = 'transparent';
-    TagCanvas.radiusX = 1;
-    TagCanvas.radiusY = 1;
-    TagCanvas.radiusZ = 1;
+    TagCanvas.radiusX = window.innerWidth * .0008;
+    TagCanvas.radiusY = window.innerWidth * .0008;
+    TagCanvas.radiusZ = window.innerWidth * .0008;
     TagCanvas.wheelZoom = false;
     TagCanvas.textHeight = 20;
     TagCanvas.reverse = true;
+    TagCanvas.shuffleTags = true;
+    TagCanvas.txtOpt = true;
+    // TagCanvas.outlineColour = 'transparent';
     // TagCanvas.shadow = '#000';
     // TagCanvas.shadowBlur = 20;
     // TagCanvas.shadowOffset = [3, 3];
-    TagCanvas.shuffleTags = true;
-    TagCanvas.txtOpt = true;
     TagCanvas.Start('myCanvas');
 
-    let widthWindow = screen.width;
-    console.log(widthWindow);
+    window.addEventListener('resize', function () {
+      let screenWidth = window.innerWidth;
+      TagCanvas.textColour = '#000';
+      TagCanvas.maxSpeed = .1;
+      TagCanvas.outlineColour = 'transparent';
+      TagCanvas.radiusX = screenWidth * .0008;
+      TagCanvas.radiusY = screenWidth * .0008;
+      TagCanvas.radiusZ = screenWidth * .0008;
 
+      TagCanvas.wheelZoom = false;
+      TagCanvas.textHeight = 20;
+      TagCanvas.reverse = true;
+      TagCanvas.shuffleTags = true;
+      TagCanvas.txtOpt = true;
+      TagCanvas.Start('myCanvas');
+    });
   } catch (e) {
     // something went wrong, hide the canvas container
     document.getElementById('myCanvasContainer').style.display = 'none';
   }
 };
 
-// window.addEventListener('resize', function() {
-//   var screenWidth = window.innerWidth;
-//   console.log('Screen width: ' + screenWidth);
-// });
 
 //* Parallax Intro Section */
 // let button = document.getElementById('container-button');
@@ -76,7 +85,7 @@ const el1 = document.getElementsByClassName('img')[0];
 const height = el1.clientHeight;
 const width = el1.clientWidth;
 let yRotation = 0, xRotation = 0, string1 = '';
-let  string2 = `
+let string2 = `
         perspective(500px)
         scale(1)
         rotateX(0)
@@ -84,7 +93,7 @@ let  string2 = `
     `
 el1.addEventListener('mousemove', (evt) => {
 
-  const {layerX, layerY} = evt;
+  const { layerX, layerY } = evt;
 
   yRotation1 = (
     (layerX - width / 2) / width
@@ -107,79 +116,79 @@ el1.addEventListener('mousemove', (evt) => {
 el1.addEventListener('mouseout', () => {
 
 
-  
+
   el1.style.transform = string2;
 });
 
 const el2 = document.getElementsByClassName('img')[1];
 el2.addEventListener('mousemove', (evt) => {
-    const {layerX, layerY} = evt;
+  const { layerX, layerY } = evt;
 
-    yRotation1 = (
-        (layerX - width / 2) / width
-    ) * 20;
+  yRotation1 = (
+    (layerX - width / 2) / width
+  ) * 20;
 
-    xRotation1 = (
-        (layerY - height / 2) / height
-    ) * 20;
+  xRotation1 = (
+    (layerY - height / 2) / height
+  ) * 20;
 
-    string1 = `
+  string1 = `
         perspective(500px)
         scale(1.1)
         rotateX(${xRotation1}deg)
         rotateY(${yRotation1}deg)
     `
 
-    el2.style.transform = string1;
+  el2.style.transform = string1;
 });
 
 el2.addEventListener('mouseout', () => {
-    el2.style.transform = string2;
+  el2.style.transform = string2;
 });
 
 const el3 = document.getElementsByClassName('img')[2];
 el3.addEventListener('mousemove', (evt) => {
-    const {layerX, layerY} = evt;
+  const { layerX, layerY } = evt;
 
-    yRotation1 = (
-        (layerX - width / 2) / width
-    ) * 20;
+  yRotation1 = (
+    (layerX - width / 2) / width
+  ) * 20;
 
-    xRotation1 = (
-        (layerY - height / 2) / height
-    ) * 20;
+  xRotation1 = (
+    (layerY - height / 2) / height
+  ) * 20;
 
-    string1 = `
+  string1 = `
         perspective(500px)
         scale(1.1)
         rotateX(${xRotation1}deg)
         rotateY(${yRotation1}deg)
     `
 
-    el3.style.transform = string1;
+  el3.style.transform = string1;
 });
 
 el3.addEventListener('mouseout', () => {
-    el3.style.transform = string2;
+  el3.style.transform = string2;
 });
 
 //* Scroll Reveal Projects Section */
 ScrollReveal({
-    reset: true,
-    distance: '60px',
-    duration: 2500,
-    delay: 400
+  reset: true,
+  distance: '60px',
+  duration: 2500,
+  delay: 400
 });
 
-ScrollReveal().reveal('.intro-section h3', {delay: 600, origin: 'left'});
-ScrollReveal().reveal('.intro-section h1, .intro-section img', {delay: 400, origin: 'right'});
-ScrollReveal().reveal('.intro-section .media-icons i', {delay: 400, origin: 'bottom', interval: 200});
-ScrollReveal().reveal('.about-section h3', {delay: 500, origin: 'right'});
-ScrollReveal().reveal('.about-section p', {delay: 500, origin: 'left'});
-ScrollReveal().reveal('.projects-section .first-horizontal-line', {delay: 300, origin: 'left', distance: '300px', duration: 2000});
-ScrollReveal().reveal('.projects-section .second-horizontal-line', {delay: 300, origin: 'right', distance: '300px', duration: 2000});
-ScrollReveal().reveal('.projects-section .project, .projects-section p', {delay: 500, origin: 'bottom', interval: 400});
-ScrollReveal().reveal('.projects-container .container-button', {delay: 600, origin: 'bottom'});
-ScrollReveal().reveal('.skills-container .experience h2', {delay: 500, origin: 'left', reset: false});
-ScrollReveal().reveal('.skills-container .skills h2', {delay: 500, origin: 'right', reset: false});
-ScrollReveal().reveal('.skills-container .vertical-divider', {delay: 700, origin: 'bottom', distance: '300px', reset: false});
+ScrollReveal().reveal('.intro-section h3', { delay: 600, origin: 'left' });
+ScrollReveal().reveal('.intro-section h1, .intro-section img', { delay: 400, origin: 'right' });
+ScrollReveal().reveal('.intro-section .media-icons i', { delay: 400, origin: 'bottom', interval: 200 });
+ScrollReveal().reveal('.about-section h3', { delay: 500, origin: 'right' });
+ScrollReveal().reveal('.about-section p', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.projects-section .first-horizontal-line', { delay: 300, origin: 'left', distance: '300px', duration: 2000 });
+ScrollReveal().reveal('.projects-section .second-horizontal-line', { delay: 300, origin: 'right', distance: '300px', duration: 2000 });
+ScrollReveal().reveal('.projects-section .project, .projects-section p', { delay: 500, origin: 'bottom', interval: 400 });
+ScrollReveal().reveal('.projects-container .container-button', { delay: 600, origin: 'bottom' });
+ScrollReveal().reveal('.skills-container .experience h2', { delay: 500, origin: 'left', reset: false });
+ScrollReveal().reveal('.skills-container .skills h2', { delay: 500, origin: 'right', reset: false });
+ScrollReveal().reveal('.skills-container .vertical-divider', { delay: 700, origin: 'bottom', distance: '300px', reset: false });
